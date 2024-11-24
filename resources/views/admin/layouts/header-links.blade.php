faaaf720744303ec0046503ea0316b8dfc991e47
Saeed Noori <noori.saeed1375@gmail.com>
2024-11-24 12:17:20 +0330

    Translate english texts to persian

--- resources/views/admin/layouts/header-links.blade.php
+++ resources/views/admin/layouts/header-links.blade.php
@@ -2,4 +2,5 @@
 <link rel="stylesheet" href="{{ asset('assets/fonts/bootstrap-icons/bootstrap-icons.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/adminlte.rtl.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/fonts/sahel/sahel.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 @yield('styles')
