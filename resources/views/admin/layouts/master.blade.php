7c41ae51d0565bad07f8b07408b733e9c539e083
Saeed Noori <noori.saeed1375@gmail.com>
2024-11-24 11:25:00 +0330

    Implement adminlte html template to project

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>@yield('title', 'NovaBlog admin')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('admin.layouts.header-links')
+</head>
+<body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
+    <div class="app-wrapper">
+        @include('admin.layouts.header')
+        @include('admin.layouts.sidebar')
+        @yield('content')
+        @include('admin.layouts.footer')
+    </div>
+    @include('admin.layouts.footer-scripts')
+</body>
+</html>
