cbc9beeb3485ce273afa758180d884231c741b16
Saeed Noori <noori.saeed1375@gmail.com>
2025-07-20 12:13:16 +0330

    Add view composer to app provider for sidebar and set dynamic value to sidebar partial

--- resources/views/front/partials/sidebar.blade.php
+++ resources/views/front/partials/sidebar.blade.php
@@ -1,17 +1,13 @@
 <div class="card mb-4">
     <div class="card-header bg-light fw-bold">دسته‌بندی‌ها</div>
     <ul class="list-group list-group-flush">
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="category.html">دسته بندی اول</a>
-            <span class="badge bg-primary rounded-pill">10</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="category.html">دسته بندی دوم</a>
-            <span class="badge bg-primary rounded-pill">10</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="category.html">دسته بندی سوم</a>
-            <span class="badge bg-primary rounded-pill">10</span>
-        </li>
+        @forelse($categories as $category)
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <a href="{{ route('category', $category->slug) }}">{{ $category->name }}</a>
+                <span class="badge bg-primary rounded-pill">{{ $category->posts->count() }}</span>
+            </li>
+        @empty
+            <p>اطلاعاتی یافت نشد!</p>
+        @endforelse
     </ul>
 </div>
