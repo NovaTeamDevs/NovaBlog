2b239c02f4642c258c7009a61bf7f3b964fb8bba
Saeed Noori <noori.saeed1375@gmail.com>
2025-07-24 16:28:30 +0330

    Add dynamic urls and data to navigation menu

--- resources/views/front/partials/navigation.blade.php
+++ resources/views/front/partials/navigation.blade.php
@@ -1,9 +1,9 @@
 <!-- Header start -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="{{ route('home') }}">
             <h1 class="d-none">نوا بلاگ</h1>
-            <img src="assets/img/nova-blog-logo.png" alt="نوا بلاگ" class="w-75">
+            <img src="{{ asset('assets/nova-blog-logo.png') }}" alt="نوا بلاگ" class="w-75">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,32 +13,40 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">خانه</a>
+                    <a class="nav-link" href="{{ route('home') }}">خانه</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">تماس با ما</a>
+                    <a class="nav-link" href="{{ route('contact.index') }}">تماس با ما</a>
                 </li>
             </ul>
 
             <!-- فرم جستجو در هدر -->
-            <form class="d-flex me-3" method="GET" action="search.html">
-                <input class="form-control me-2" type="search" placeholder="جستجو...">
+            <form class="d-flex me-3" method="GET" action="{{ route('search') }}">
+                <input class="form-control me-2" type="search" name="q" placeholder="جستجو..." value="{{ request()->get('q') }}">
                 <button class="btn btn-outline-primary" type="submit">جستجو</button>
             </form>
 
             <ul class="navbar-nav">
-                <!--
-                <li class="nav-item"><a class="nav-link" href="#">داشبورد</a></li>
-                <li class="nav-item">
-                    <form method="POST" action="#">
-                        <button type="submit" class="nav-link btn btn-link" style="display:inline; cursor:pointer;">
-                            خروج
-                        </button>
-                    </form>
-                </li>
-                -->
-                <li class="nav-item"><a class="nav-link" href="#">ورود</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">ثبت‌نام</a></li>
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('admin.dashboard') }}">{{ auth()->user()->full_name }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="nav-link btn btn-link" style="display:inline; cursor:pointer;">خروج</button>
+                        </form>
+                    </li>
+                @endauth
+
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">ورود</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">ثبت نام</a>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
